20a580be9ea7079ead576878d53b06982de7057a
Jan0211 <jan.pixberg123@gmail.com>
2023-01-23 18:14:02 +0100

    added method for adding new device tiles

--- app/src/main/java/de/rehatech2223/lgg_frontend/ui/main/FragmentDevices.kt
+++ app/src/main/java/de/rehatech2223/lgg_frontend/ui/main/FragmentDevices.kt
@@ -5,9 +5,17 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
+import android.widget.TextView
 import de.rehatech2223.lgg_frontend.R
+import org.jetbrains.annotations.NotNull
+import org.jetbrains.annotations.Nullable
 
 class FragmentDevices : Fragment() {
+
+    var currentLinearLayout: LinearLayout? = null;
+    var mainLinearLayout: LinearLayout? = null;
+    var addedDevicesCount: Int = 0;
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,6 +30,52 @@ class FragmentDevices : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_devices, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        mainLinearLayout = view.findViewById<LinearLayout>(R.id.linearLayout);
+
+        addNewDevicesTile("Bild", "Lampe");
+        addNewDevicesTile("Bild2", "Lampe2");
+        addNewDevicesTile("Bild3", "Lampe3");
+
+        addNewDevicesTile("Bild", "Lampe");
+        addNewDevicesTile("Bild2", "Lampe2");
+        addNewDevicesTile("Bild3", "Lampe3");
+
+        addNewDevicesTile("Bild", "Lampe");
+        addNewDevicesTile("Bild2", "Lampe2");
+        addNewDevicesTile("Bild3", "Lampe3");
+
+        addNewDevicesTile("Bild", "Lampe");
+        addNewDevicesTile("Bild2", "Lampe2");
+        addNewDevicesTile("Bild3", "Lampe3");
+
+        addNewDevicesTile("Bild", "Lampe");
+        addNewDevicesTile("Bild2", "Lampe2");
+        addNewDevicesTile("Bild3", "Lampe3");
+
+        addNewDevicesTile("Bild", "Lampe");
+        addNewDevicesTile("Bild2", "Lampe2");
+        addNewDevicesTile("Bild3", "Lampe3");
+
+        addNewDevicesTile("Bild", "Lampe");
+        addNewDevicesTile("Bild2", "Lampe2");
+        addNewDevicesTile("Bild3", "Lampe3");
+    }
+
+    fun addNewDevicesTile(image: String, name: String) {
+        if(addedDevicesCount % 3 == 0) {
+            currentLinearLayout = layoutInflater.inflate(R.layout.devices_view, null).findViewById(R.id.linearLayout);
+            mainLinearLayout?.addView(currentLinearLayout);
+        }
+        var newDeviceTile = layoutInflater.inflate(R.layout.devices_tile, null);
+        newDeviceTile.findViewById<TextView>(R.id.name).text = name;
+        newDeviceTile.findViewById<TextView>(R.id.image).text = image;
+        currentLinearLayout?.addView(newDeviceTile);
+        addedDevicesCount++;
     }
 
     companion object {
