d780870eee8035eb51942246dbda82958b77c745
KryM8 <fynndebus@googlemail.com>
2023-01-25 18:32:13 +0100

    Added Functionality to Switch, Started work on Color Themes

--- app/src/main/java/de/rehatech2223/lgg_frontend/MainActivity.kt
+++ app/src/main/java/de/rehatech2223/lgg_frontend/MainActivity.kt
@@ -8,6 +8,11 @@ import androidx.viewpager.widget.ViewPager
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
+import android.widget.CompoundButton
+import android.widget.TableLayout
+import androidx.appcompat.widget.SwitchCompat
+import com.google.android.material.tabs.TabLayout.Tab
 import de.rehatech2223.lgg_frontend.ui.main.SectionsPagerAdapter
 import de.rehatech2223.lgg_frontend.databinding.ActivityMainBinding
 
@@ -16,6 +21,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        //setTheme(R.style.TestTheme)
         super.onCreate(savedInstanceState)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -26,5 +32,19 @@ class MainActivity : AppCompatActivity() {
         viewPager.adapter = sectionsPagerAdapter
         val tabs: TabLayout = binding.tabs
         tabs.setupWithViewPager(viewPager)
+        prepareSwitchFunctionality(tabs)
+    }
+
+    private fun prepareSwitchFunctionality(tabs: TabLayout){
+        val optionsTab: Tab? = tabs.getTabAt(2)
+        optionsTab!!.view.visibility = View.GONE
+        val switch: SwitchCompat = binding.switch1
+        switch.setOnCheckedChangeListener { _, c ->
+            if(!c) optionsTab.view.visibility = View.VISIBLE
+            else {
+                if(optionsTab.isSelected) tabs.selectTab(tabs.getTabAt(0))
+                optionsTab.view.visibility = View.GONE
+            }
+        }
     }
 }
