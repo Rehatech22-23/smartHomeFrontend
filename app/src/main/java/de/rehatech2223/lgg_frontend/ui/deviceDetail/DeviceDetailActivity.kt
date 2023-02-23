31c333e2180977b1908f2b86c903395671ad1c99
KryM8 <fynndebus@googlemail.com>
2023-02-23 17:37:55 +0100

    Added templates for device-detail-functions and added OnOffFunction

--- app/src/main/java/de/rehatech2223/lgg_frontend/ui/deviceDetail/DeviceDetailActivity.kt
+++ app/src/main/java/de/rehatech2223/lgg_frontend/ui/deviceDetail/DeviceDetailActivity.kt
@@ -1,14 +1,15 @@
 package de.rehatech2223.lgg_frontend.ui.deviceDetail
 
 import android.os.Bundle
-import android.util.Log
-import android.view.MenuItem
-import android.widget.Button
+import android.widget.ImageView
+import android.widget.LinearLayout
 import android.widget.TextView
 import de.rehatech2223.datamodel.DeviceDTO
+import de.rehatech2223.datamodel.FunctionDTO
 import de.rehatech2223.lgg_frontend.DynamicThemeActivity
 import de.rehatech2223.lgg_frontend.R
 import de.rehatech2223.lgg_frontend.services.ServiceProvider
+import de.rehatech2223.lgg_frontend.util.TileImageUtil
 
 class DeviceDetailActivity : DynamicThemeActivity() {
 
@@ -20,29 +21,41 @@ class DeviceDetailActivity : DynamicThemeActivity() {
         setContentView(R.layout.device_detail_activity)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
-
         val deviceId: String = intent.getStringExtra("deviceId")!!
         val deviceArgument: DeviceDTO? = ServiceProvider.devicesService.getDeviceInfo(deviceId)
         deviceDTO = deviceArgument ?: DeviceDTO("Error Device", "-1", ArrayList())
 
-        Log.d("handler", "hewwow , ${deviceDTO.deviceName}")
-        Log.d("handler", "is showing: ${supportActionBar?.isShowing}")
-
-        findViewById<TextView>(R.id.back_text).setOnClickListener {
-            Log.d("handler", "clicked button")
-            finish()
-        }
+        initMainViewElements()
+        initFunctions()
     }
 
-    // this event will enable the back
-    // function to the button on press
-    override fun onContextItemSelected(item: MenuItem): Boolean {
-        when (item.itemId) {
-            android.R.id.home -> {
-                finish()
-                return true
-            }
+    private fun initMainViewElements(){
+        findViewById<TextView>(R.id.back_text).setOnClickListener {
+            finish()
         }
-        return super.onContextItemSelected(item)
+
+        findViewById<TextView>(R.id.overview_name).text = deviceDTO.deviceName
+        findViewById<ImageView>(R.id.overview_image)
+            .setImageResource(TileImageUtil.getDeviceImageId(deviceDTO, this))
+    }
+
+    private fun initFunctions(){
+        val scrollLayout = findViewById<LinearLayout>(R.id.scroll_layout)
+        for (functionId in deviceDTO.functionIds){
+            val functionDTO: FunctionDTO = ServiceProvider.functionService
+                .getFunctionInfo(functionId) ?: continue
+
+            val functionView = if (functionDTO.rangeDTO != null){
+                FunctionRange(this, null, functionDTO, deviceDTO)
+            }else if (functionDTO.onOff != null){
+                FunctionOnOff(this, null, functionDTO, deviceDTO)
+            }else if (functionDTO.outputValue != null){
+                FunctionOutput(this, null, functionDTO, deviceDTO)
+            }else if (functionDTO.outputTrigger != null){
+                FunctionTrigger(this, null, functionDTO, deviceDTO)
+            }else continue
+
+            scrollLayout.addView(functionView)
+        }
     }
 }
---
+++ app/src/main/java/de/rehatech2223/lgg_frontend/ui/deviceDetail/FunctionTrigger.kt
@@ -0,0 +1,17 @@
+package de.rehatech2223.lgg_frontend.ui.deviceDetail
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.LayoutInflater
+import android.widget.LinearLayout
+import de.rehatech2223.datamodel.DeviceDTO
+import de.rehatech2223.datamodel.FunctionDTO
+import de.rehatech2223.lgg_frontend.R
+
+class FunctionTrigger(context: Context, attrs: AttributeSet? = null, functionDTO: FunctionDTO, deviceDTO: DeviceDTO): LinearLayout(context, attrs) {
+
+    init {
+        LayoutInflater.from(context).inflate(R.layout.function_trigger, this, true)
+        orientation = VERTICAL
+    }
+}
